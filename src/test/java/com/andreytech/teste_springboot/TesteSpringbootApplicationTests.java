eeffb3973e613d46de63c19bcc967dcc8b8cda95
AndreyNicollas <adsandr3y24@gmail.com>
2025-07-04 05:18:30 -0300

    Projeto springboot criado

---
+++ src/test/java/com/andreytech/teste_springboot/TesteSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreytech.teste_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
