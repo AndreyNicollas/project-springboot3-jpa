0264756da8e3e7054f2e2e2e388f27fd8c20e91a
AndreyNicollas <adsandr3y24@gmail.com>
2025-07-06 16:02:41 -0300

    Payment, one-to-one association

--- src/main/java/com/andreytech/teste_springboot/entities/Order.java
+++ src/main/java/com/andreytech/teste_springboot/entities/Order.java
@@ -2,7 +2,6 @@ package com.andreytech.teste_springboot.entities;
 
 import com.andreytech.teste_springboot.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -31,6 +30,9 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -74,6 +76,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
